2b9bd84bded771060c7f8dfb6ff50a35ec45e769
Diego Pereira <diego@develop.diego.develop.com.br>
2022-05-20 11:59:46 -0300

    Programação Orientada a Objetos em JS

--- 02-variaveis/03-array.js
+++
@@ -1,4 +0,0 @@
-//O array começa da posição 0
-const semanas = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
-
-console.log(semanas[2]);
--- 03-operadores/04-logico.js
+++
@@ -1,9 +0,0 @@
-//Operador and
-console.log(true && true);
-
-//Operador or
-console.log(true || false);
-
-//Operador not
-console.log(!true);
-
